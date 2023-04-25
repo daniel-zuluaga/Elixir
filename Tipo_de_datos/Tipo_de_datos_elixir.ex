bac15089ce6b4715799ba02ff5811b42b4b48e31
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2023-04-24 22:16:31 -0500

    [Feature/Add] add the type data in elixir

---
+++ Tipo_de_datos/Tipo_de_datos_elixir.ex
@@ -0,0 +1,56 @@
+# Tipos de datos
+
+## Section
+
+```elixir
+´´´Elixir
+1 # integer
+1.4 # float o double
+true # boolean
+[1,2,45,909] # listas
+{1,23,45,90,890} # tuplas
+"Elixir" # strings
+:atom # atom or symbol
+´´´
+```
+
+## Arítmeticas
+
+```elixir
+2 + 3 # RTA/ 5
+```
+
+```elixir
+4 / 5 # RTA/ 0.8
+```
+
+```elixir
+10 / 2 # RTA/ 5.0
+```
+
+```elixir
+div(10, 5) # RTA/ 2
+```
+
+```elixir
+rem(10, 2) # RTA/ 2
+```
+```elixir
+rem(10, 6) # RTA/ 4
+```
+
+```elixir
+round(2.58) # RTA/ 3
+```
+
+```elixir
+trunc(3.6) # RTA/ 3
+```
+
+```elixir
+round(2.4) # RTA/ 2
+```
+
+```elixir
+trunc(3.4) # RTA/ 3
+```
