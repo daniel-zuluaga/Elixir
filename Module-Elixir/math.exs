0cf8f7e4a0c12d01f36dd325862008afc2c9e6cb
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2023-04-28 10:07:19 -0500

    [Feature/add] add the modules elixir

---
+++ Module-Elixir/math.exs
@@ -0,0 +1,3 @@
+
+IO.inspect(MathMo.rest(120,82))
+IO.inspect(MathMo.sum(20,12))
---
+++ Module-Elixir/math_module.ex
@@ -0,0 +1,11 @@
+
+defmodule MathMo do
+  def rest(a, b) do
+    IO.puts("Esta es la resta de #{a} y #{b}")
+    a - b
+  end
+  def sum(r, t) do
+    IO.puts("Esta es la suma de #{r} y #{t}")
+    r + t
+  end
+end
